2c188dfdf3c27b624274777c40a76565c6b078f9
cuong nguyen <cuongpianna1996@gmail.com>
2021-01-07 07:43:47 +0700

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,49 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <trading-vue titleTxt="Trading" :data="this.$data"></trading-vue>
   </div>
 </template>
 
 <script>
+import TradingVue from 'trading-vue-js'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    TradingVue
+  },
+  mounted() {
+    this.run()
+  },
+  data() {
+    return {
+      ohlcv: [
+        [ 1551128400000, 33,  37.1, 14,  14,  196 ],
+        [ 1551132000000, 13.7, 30, 6.6,  30,  206 ],
+        [ 1551135600000, 29.9, 33, 21.3, 21.8, 74 ],
+        [ 1551139200000, 21.7, 25.9, 18, 24,  140 ],
+        [ 1551142800000, 24.1, 24.1, 24, 24.1, 29 ],
+      ]
+    }
+  },
+  methods: {
+    run() {
+      var self = this
+      setInterval(function (){
+        console.log('cuong')
+        let  a = 24.1;
+        let  b = 24.1;
+        let  c = 24;
+        let  d = 24.1;
+        let  e = 29;
+        let latsIndex = self.ohlcv[self.ohlcv.length - 1][0]
+        console.log(latsIndex)
+        console.log(self.ohlcv.length)
+        self.ohlcv.push([latsIndex + 1000000, a, b, c, d ,e])
+      }, 2000)
+    }
   }
 }
 </script>
